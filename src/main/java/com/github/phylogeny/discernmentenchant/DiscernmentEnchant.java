7e9796df695f489696621342f32869dc9371b0a8
Phylogeny <molecularphylo@gmail.com>
2020-07-07 10:08:21 -0700

    Added discernment effect.

--- src/main/java/com/github/phylogeny/discernmentenchant/DiscernmentEnchant.java
+++ src/main/java/com/github/phylogeny/discernmentenchant/DiscernmentEnchant.java
@@ -6,7 +6,10 @@ import net.minecraft.enchantment.EnchantmentType;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.potion.Effect;
+import net.minecraft.potion.EffectType;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
@@ -21,11 +24,15 @@ public class DiscernmentEnchant
 {
     public static final String MOD_ID = "discernmentenchant";
     private static final DeferredRegister<Enchantment> ENCHANTMENTS = DeferredRegister.create(ForgeRegistries.ENCHANTMENTS, MOD_ID);
-    private static final RegistryObject<Enchantment> DISCERNMENT = ENCHANTMENTS.register("discernment", DiscernmentEnchantment::new);
+    private static final DeferredRegister<Effect> EFFECTS = DeferredRegister.create(ForgeRegistries.POTIONS, MOD_ID);
+    private static final RegistryObject<Enchantment> DISCERNMENT_ENCHANT = ENCHANTMENTS.register("discernment", DiscernmentEnchantment::new);
+    private static final RegistryObject<Effect> DISCERNMENT_EFFECT = EFFECTS.register("discernment", DiscernmentEffect::new);
 
     public DiscernmentEnchant()
     {
-        ENCHANTMENTS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        ENCHANTMENTS.register(bus);
+        EFFECTS.register(bus);
     }
 
     @SubscribeEvent
@@ -38,9 +45,10 @@ public class DiscernmentEnchant
         if (!(source instanceof LivingEntity))
             return;
 
-        if (source != null && !source.isSneaking()
-                && DISCERNMENT.get().getEntityEquipment((LivingEntity) source).values().stream()
-                    .anyMatch(stack -> EnchantmentHelper.getEnchantmentLevel(DISCERNMENT.get(), stack) > 0))
+        LivingEntity entity = (LivingEntity) source;
+        if (source != null && !source.isSneaking() && (entity.isPotionActive(DISCERNMENT_EFFECT.get())
+                || DISCERNMENT_ENCHANT.get().getEntityEquipment(entity).values().stream()
+                    .anyMatch(stack -> EnchantmentHelper.getEnchantmentLevel(DISCERNMENT_ENCHANT.get(), stack) > 0)))
             event.setCanceled(true);
     }
 
@@ -49,6 +57,14 @@ public class DiscernmentEnchant
         protected DiscernmentEnchantment()
         {
             super(Rarity.UNCOMMON, EnchantmentType.VANISHABLE, EquipmentSlotType.values());
+        }
+    }
+
+    private static class DiscernmentEffect extends Effect
+    {
+        protected DiscernmentEffect()
+        {
+            super(EffectType.BENEFICIAL, 0);//TODO color
         }
     }
 }
