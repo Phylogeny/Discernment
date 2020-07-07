37eb2fe5051a99e8290d35dde13eea388a5cec54
Phylogeny <molecularphylo@gmail.com>
2020-07-07 10:08:41 -0700

    Added discernment potions.

--- src/main/java/com/github/phylogeny/discernmentenchant/DiscernmentEnchant.java
+++ src/main/java/com/github/phylogeny/discernmentenchant/DiscernmentEnchant.java
@@ -7,7 +7,9 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.potion.Effect;
+import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.EffectType;
+import net.minecraft.potion.Potion;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -25,14 +27,26 @@ public class DiscernmentEnchant
     public static final String MOD_ID = "discernmentenchant";
     private static final DeferredRegister<Enchantment> ENCHANTMENTS = DeferredRegister.create(ForgeRegistries.ENCHANTMENTS, MOD_ID);
     private static final DeferredRegister<Effect> EFFECTS = DeferredRegister.create(ForgeRegistries.POTIONS, MOD_ID);
+    private static final DeferredRegister<Potion> POTIONS = DeferredRegister.create(ForgeRegistries.POTION_TYPES, MOD_ID);
     private static final RegistryObject<Enchantment> DISCERNMENT_ENCHANT = ENCHANTMENTS.register("discernment", DiscernmentEnchantment::new);
     private static final RegistryObject<Effect> DISCERNMENT_EFFECT = EFFECTS.register("discernment", DiscernmentEffect::new);
+    static
+    {
+        registerDiscernmentPotion("discernment", 3600);
+        registerDiscernmentPotion("long_discernment", 9600);
+    }
 
     public DiscernmentEnchant()
     {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
         ENCHANTMENTS.register(bus);
         EFFECTS.register(bus);
+        POTIONS.register(bus);
+    }
+
+    private static void registerDiscernmentPotion(String name, int duration)
+    {
+        POTIONS.register(name, () -> new Potion(new EffectInstance(DISCERNMENT_EFFECT.get(), duration)));
     }
 
     @SubscribeEvent
