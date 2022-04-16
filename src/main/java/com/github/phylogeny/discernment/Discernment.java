467d66b9e1dc8d254ca3d9a04c250a40f75e0b33
Phylogeny <molecularphylo@gmail.com>
2022-04-16 11:19:27 -0700

    Updated to MC 1.17.1 and switched to using official method/class names and using parchment for parameter names.

--- src/main/java/com/github/phylogeny/discernment/Discernment.java
+++ src/main/java/com/github/phylogeny/discernment/Discernment.java
@@ -1,27 +1,31 @@
 package com.github.phylogeny.discernment;
 
 import com.google.common.base.Stopwatch;
-import net.minecraft.enchantment.Enchantment;
-import net.minecraft.enchantment.EnchantmentHelper;
-import net.minecraft.enchantment.EnchantmentType;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.inventory.EquipmentSlotType;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
-import net.minecraft.item.crafting.Ingredient;
-import net.minecraft.potion.*;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.alchemy.Potion;
+import net.minecraft.world.item.alchemy.PotionUtils;
+import net.minecraft.world.item.alchemy.Potions;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentCategory;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraftforge.common.brewing.BrewingRecipeRegistry;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.DeferredWorkQueue;
-import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
@@ -34,10 +38,10 @@ public class Discernment
 {
     public static final String MOD_ID = "discernment";
     private static final DeferredRegister<Enchantment> ENCHANTMENTS = DeferredRegister.create(ForgeRegistries.ENCHANTMENTS, MOD_ID);
-    private static final DeferredRegister<Effect> EFFECTS = DeferredRegister.create(ForgeRegistries.POTIONS, MOD_ID);
-    private static final DeferredRegister<Potion> POTIONS = DeferredRegister.create(ForgeRegistries.POTION_TYPES, MOD_ID);
+    private static final DeferredRegister<MobEffect> EFFECTS = DeferredRegister.create(ForgeRegistries.MOB_EFFECTS, MOD_ID);
+    private static final DeferredRegister<Potion> POTIONS = DeferredRegister.create(ForgeRegistries.POTIONS, MOD_ID);
     private static final RegistryObject<Enchantment> DISCERNMENT_ENCHANT = ENCHANTMENTS.register("discernment", DiscernmentEnchantment::new);
-    private static final RegistryObject<Effect> DISCERNMENT_EFFECT = EFFECTS.register("discernment", DiscernmentEffect::new);
+    private static final RegistryObject<MobEffect> DISCERNMENT_EFFECT = EFFECTS.register("discernment", DiscernmentEffect::new);
     private static final RegistryObject<Potion> DISCERNMENT_POTION = registerDiscernmentPotion("discernment", 3600);
     private static final RegistryObject<Potion> DISCERNMENT_POTION_LONG = registerDiscernmentPotion("long_discernment", 9600);
 
@@ -52,42 +56,39 @@ public class Discernment
 
     private void setup(FMLCommonSetupEvent event)
     {
-        DeferredWorkQueue.runLater(() ->
-        {
-            addBrewingRecipe(Potions.AWKWARD, Items.EMERALD, DISCERNMENT_POTION);
-            addBrewingRecipe(DISCERNMENT_POTION.get(), Items.REDSTONE, DISCERNMENT_POTION_LONG);
-        });
+        addBrewingRecipe(Potions.AWKWARD, Items.EMERALD, DISCERNMENT_POTION);
+        addBrewingRecipe(DISCERNMENT_POTION.get(), Items.REDSTONE, DISCERNMENT_POTION_LONG);
     }
 
     private void addBrewingRecipe(Potion input, Item ingredient, RegistryObject<Potion> output)
     {
-        BrewingRecipeRegistry.addRecipe(Ingredient.fromStacks(getPotionStack(input)), Ingredient.fromItems(ingredient), getPotionStack(output.get()));
+        BrewingRecipeRegistry.addRecipe(Ingredient.of(getPotionStack(input)), Ingredient.of(ingredient), getPotionStack(output.get()));
     }
 
     private ItemStack getPotionStack(Potion potion)
     {
-        return PotionUtils.addPotionToItemStack(new ItemStack(Items.POTION), potion);
+        return PotionUtils.setPotion(new ItemStack(Items.POTION), potion);
     }
 
     private static RegistryObject<Potion> registerDiscernmentPotion(String name, int duration)
     {
-        return POTIONS.register(name, () -> new Potion(new EffectInstance(DISCERNMENT_EFFECT.get(), duration)));
+        return POTIONS.register(name, () -> new Potion(new MobEffectInstance(DISCERNMENT_EFFECT.get(), duration)));
     }
 
     @SubscribeEvent
     public static void discern(LivingAttackEvent event)
     {
-        if (!event.getEntityLiving().getType().getClassification().getPeacefulCreature())
+        if (!event.getEntityLiving().getType().getCategory().isFriendly())
             return;
 
-        Entity source = event.getSource().getTrueSource();
+        Entity source = event.getSource().getEntity();
         if (!(source instanceof LivingEntity))
             return;
 
         LivingEntity entity = (LivingEntity) source;
-        if (source != null && !source.isSneaking() && (entity.isPotionActive(DISCERNMENT_EFFECT.get())
-                || DISCERNMENT_ENCHANT.get().getEntityEquipment(entity).values().stream()
-                    .anyMatch(stack -> EnchantmentHelper.getEnchantmentLevel(DISCERNMENT_ENCHANT.get(), stack) > 0)))
+        if (source != null && !source.isShiftKeyDown() && (entity.hasEffect(DISCERNMENT_EFFECT.get())
+                || DISCERNMENT_ENCHANT.get().getSlotItems(entity).values().stream()
+                    .anyMatch(stack -> EnchantmentHelper.getItemEnchantmentLevel(DISCERNMENT_ENCHANT.get(), stack) > 0)))
             event.setCanceled(true);
     }
 
@@ -95,11 +96,11 @@ public class Discernment
     {
         protected DiscernmentEnchantment()
         {
-            super(Rarity.UNCOMMON, EnchantmentType.VANISHABLE, EquipmentSlotType.values());
+            super(Rarity.UNCOMMON, EnchantmentCategory.VANISHABLE, EquipmentSlot.values());
         }
     }
 
-    private static class DiscernmentEffect extends Effect
+    private static class DiscernmentEffect extends MobEffect
     {
         private final Stopwatch timer;
         private static final int CYCLE_TIME = 2000;
@@ -111,12 +112,12 @@ public class Discernment
 
         protected DiscernmentEffect()
         {
-            super(EffectType.BENEFICIAL, 0);
+            super(MobEffectCategory.BENEFICIAL, 0);
             timer = Stopwatch.createStarted();
         }
 
         @Override
-        public int getLiquidColor()
+        public int getColor()
         {
             return COLORS[(int) (timer.elapsed(TimeUnit.MILLISECONDS) % CYCLE_TIME)];
         }
