d350aa9fa78d5e5aad16e4d9bca3bfe93eb0f836
Phylogeny <molecularphylo@gmail.com>
2022-04-16 13:02:30 -0700

    Updated to MC 1.18.2

--- src/main/java/com/github/phylogeny/discernment/Discernment.java
+++ src/main/java/com/github/phylogeny/discernment/Discernment.java
@@ -25,9 +25,9 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
